086b01a4e785a5d4f7eefdb78454da117e4c4133
Gene Laisne <glaisne@carbonite.com>
2021-03-10 21:29:40 -0500

    Initial push

---
+++ Deploy-AzTemplate.ps1
@@ -0,0 +1,44 @@
+<#
+.Synopsis
+Short description
+.DESCRIPTION
+Long description
+.EXAMPLE
+Example of how to use this cmdlet
+.EXAMPLE
+Another example of how to use this cmdlet
+#>
+
+
+#-----------------------------
+# Parameters
+#-----------------------------
+
+[CmdletBinding()]
+Param
+(
+    # Param1 help description
+    [Parameter(Mandatory=$true,
+            Position=0)]
+    [string]
+    $ResourceGroupName,
+
+    # Param2 help description
+    [string]
+    $Location = 'eastus2'
+)
+
+#-----------------------------
+# Variables
+#-----------------------------
+
+
+#-----------------------------
+# Functions
+#-----------------------------
+
+
+#-----------------------------
+# Main
+#-----------------------------
+
